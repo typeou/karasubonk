b25bcf5871e63263d6952b2dabdd5ee15cd5e625
typeou <typeou@typeou.dev>
2025-05-30 09:46:08 -0600

    Updated some status descriptions.

--- renderer.js
+++ renderer.js
@@ -28,16 +28,16 @@ const statusTitle = [
 const statusDesc = [
     "",
     "<p>Click the \"Log in\" button below to open a Twitch authentication window in your browser.</p>",
-    "<p>If this message doesn't disappear after a few seconds, please refresh the KBonk Browser Source in OBS.</p><p>The KBonk Browser Source should be active with <mark>karasubonk/resources/app/bonker.html</mark> as the source file.</p><p>If you have a Custom Browser Source IP set in the settings, please ensure it is correct.</p>",
+    "<p>If this message doesn't disappear after a few seconds, please refresh the \"bonker.html\" Browser Source in OBS.</p><p>The Browser Source should be active with <mark>(KBonk folder)/resources/app/bonker.html</mark> as the source file.</p><p>If you have a Custom Browser Source IP set in the settings, please ensure it is correct.</p>",
     "<p>Please use VTube Studio to position your model's head under the guide being displayed in OBS.</p><p>Your VTube Studio Source and KBonk Browser Source should be overlapping.</p><p>Press the <mark>Continue Calibration</mark> button below to continue to the next step.</p>",
     "<p>Please use VTube Studio to position your model's head under the guide being displayed in OBS.</p><p>Your VTube Studio Source and KBonk Browser Source should be overlapping.</p><p>Press the <mark>Confirm Calibration</mark> button below to finish calibration.</p>",
-    [ "<p>If this message doesn't disappear after a few seconds, please refresh the KBonk Browser Source.</p><p>If that doesn't work, please ensure the VTube Studio API is enabled on port <mark>", "</mark>.</p><p>If you have a Custom VTube Studio IP set in the settings, please ensure it is correct.</p>" ],
-    "<p>Please use the Channel Point Reward you'd like to use.</p>",
+    [ "<p>If this message doesn't disappear after a few seconds, please refresh the \"bonker.html\" Browser Source.</p><p>If that doesn't work, please ensure the VTube Studio API is enabled on port <mark>", "</mark>.</p><p>If you have a Custom VTube Studio IP set in the settings, please ensure it is correct.</p>" ],
+    "<p>Please redeem the Channel Point Reward you'd like to use.</p>",
     "<p>This short process will decide the impact location of thrown objects.</p><p>Please click \"Start Calibration\" to start the calibration process.</p>",
     "<p>This should just take a moment.</p>",
     [ "<p>The port <mark>", "</mark> is already in use. Another process may be using this port.</p><p>Try changing the Browser Source Port in Settings, under Advanced Settings.</p><p>It should be some number between 1024 and 65535.</p>" ],
-    "<p>KBonk and the Browser Source are running on different versions.</p><p>Please ensure KBonk and the Browser Source are both running from the same folder.</p>",
-    "<p>No version response from Browser Source.</p><p>KBonk and the Browser Source may be running on different versions.</p><p>Please ensure KBonk and the Browser Source are both running from the same folder.</p>",
+    "<p>KBonk and the \"bonker.html\" Browser Source are running on different versions.</p><p>This probably means that you have more than one KBonk folder.</p><p>Please ensure KBonk and the \"bonker.html\" Browser Source are both running from the same folder.</p>",
+    "<p>No version response from \"bonker.html\" Browser Source.</p><p>KBonk and the Browser Source may be running on different versions.</p><p>This probably means that you have more than one KBonk folder.</p><p>Please ensure KBonk and the \"bonker.html\" Browser Source are both running from the same folder.</p>",
     "<p>Awaiting authentication response from browser...</p>"
 ];
 
